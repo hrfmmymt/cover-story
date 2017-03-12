e58faedf7728b1e52f84934c0e5bf46ef219f475
hrfmmymt <hrfmmymt@gmail.com>
2017-03-12 22:58:21 +0900

    modified

--- src/components/article.js
+++ src/components/article.js
@@ -1,6 +1,41 @@
 import React from 'react'
+import ScrollAnim from 'rc-scroll-anim'
+
+const ScrollParallax = ScrollAnim.Parallax
+const ScrollElement = ScrollAnim.Element
 
 export default class Article extends React.Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      css: {
+        backgroundColor: '#174270',
+        height: 920
+      },
+      cssNoPosition: true
+    }
+  }
+
+  onComplete = (e) => {
+    console.log(e)
+  }
+
+  setCss = (e) => {
+    const css = this.state.css
+    console.log(e)
+    if (this.state.cssNoPosition) {
+      css.position = 'fixed'
+      css.top = 0
+    } else {
+      css.position = ''
+      css.top = ''
+    }
+    this.setState({
+      css,
+      cssNoPosition: !this.state.cssNoPosition
+    })
+  }
+
   componentWillMount() {
     if (this.props.didMount) {
       this.props.actions.meta.set({
@@ -16,6 +51,240 @@ export default class Article extends React.Component {
     return (
       <div className="home">
         <h2>This page is /</h2>
+
+        {
+        /****
+         Static view
+        ****/ }
+        <div className="pack-page page0 ">
+          <div className="home-title">
+            <div className="page-title">
+              <p>Title 1</p>
+            </div>
+            <div className="page-description" key="c">
+              <p>The parallax demo</p>
+            </div>
+          </div>
+        </div>
+
+        {
+        /****
+         Scroll Effects 1st. view
+        ****/ }
+        <div
+          className="pack-page"
+          style={{
+            backgroundColor: '#174270',
+            position: 'relative',
+            overflow: 'hidden',
+            height: 1600
+          }}
+        >
+
+          <ScrollParallax
+            animation={{ opacity: 1 }}
+            always={false}
+            style={{ opacity: 0, paddingTop: 60 }}
+            className="tween-one"
+          >
+            ボヤーン
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ opacity: 1 }}
+            style={{ opacity: 0, paddingTop: 0, fontSize: 24 }}
+            className="tween-one"
+          >
+            ボヤーン
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ scaleX: 1, scaleY: 1 }}
+            style={{ transform: 'scale(0)', color: '#fff' }}
+            className="demo parallax-shape"
+          >
+            ドーン
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ rotate: 360 }}
+            style={{ transform: 'rotate(0)' }}
+            className="demo2 parallax-shape"
+          >
+            ぐるぐる
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ x: 0, opacity: 1 }}
+            style={{
+              transform: 'translateX(-200px)',
+              opacity: 0,
+              backgroundColor: '#133252',
+              color: '#fff'
+            }}
+            className="demo2 parallax-shape"
+          >
+            スッ
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ rotateY: 360 }}
+            style={{ transform: 'perspective(100px) rotateY(0)', backgroundColor: '#0098CE' }}
+            className="demo2 parallax-shape"
+          >
+            ヒュン
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ opacity: 1 }}
+            style={{ opacity: 0, paddingTop: 60, fontSize: 24 }}
+            className="tween-one"
+          >
+            aaaaa
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ blur: '0px', playScale: [0.5, 0.8], opacity: 1 }}
+            style={{ filter: 'blur(20px)', opacity: 0, color: '#fff' }}
+            className="demo parallax-shape"
+          >
+            モヤア
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ color: '#fff000', backgroundColor: '#F38EAD', playScale: [0.3, 0.8] }}
+            style={{ filter: 'blur(0px)', color: '#fff' }}
+            className="demo parallax-shape"
+          >
+            変色
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={{ opacity: 1 }}
+            style={{ opacity: 0, paddingTop: 60, fontSize: 24 }}
+            className="tween-one"
+          >
+            bbbbb
+          </ScrollParallax>
+
+          <ScrollParallax
+            animation={[
+              { opacity: 1, playScale: [0, 0.2] },
+              { blur: '0px', backgroundColor: '#F38EAD', playScale: [0, 0.2] },
+              {
+                translateX: -100,
+                boxShadow: '5px 5px 5px #000',
+                color: '#fff000',
+                playScale: [0, 0.2]
+              },
+              { translateX: 100, playScale: [0, 0.2] },
+              { translateX: 0, playScale: [0, 0.2] }
+            ]}
+            style={{ filter: 'blur(10px)', transform: 'translateX(0px)', opacity: 0, color: '#fff' }}
+            className="demo parallax-shape"
+          >
+            ヘイヘイ
+          </ScrollParallax>
+        </div>
+
+        {
+        /****
+         Scroll Effects 2nd. view
+        ****/ }
+        <div className="pack-page"
+          style={{
+            backgroundColor: '#0097D0',
+            position: 'relative',
+            textAlign: 'center',
+            fontSize: 36
+          }}
+        >
+          <div className="page2-title">Title 2</div>
+          <ScrollParallax
+            animation={{ translateY: 120, opacity: 1, ease: 'linear', playScale: [0, 2] }}
+            style={{ transform: 'translateY(420px) scale(.8)', color: '#fff' }}
+          >
+            <img
+              src="https://os.alipayobjects.com/rmsportal/CrVCkwvtTQQvQHL.png"
+              style={{ width: 100 }}
+            />
+          </ScrollParallax>
+          <ScrollParallax
+            animation={{ translateY: 20, opacity: 1, ease: 'linear', playScale: [0, 1.5] }}
+            style={{ transform: 'translateY(380px) scale(.9)', color: '#fff' }}
+          >
+            <img
+              src="https://os.alipayobjects.com/rmsportal/sfmdyWNlweIvfUh.png"
+              style={{ width: 150 }}
+            />
+          </ScrollParallax>
+          <ScrollParallax
+            animation={{ translateY: -100, opacity: 1, ease: 'linear', playScale: [0, 1] }}
+            style={{ transform: 'translateY(320px)', color: '#fff' }}
+          >
+            <img
+              src="https://os.alipayobjects.com/rmsportal/sfmdyWNlweIvfUh.png"
+              style={{ width: 200 }}
+            />
+          </ScrollParallax>
+        </div>
+
+        {
+        /****
+         Scroll Effects last view
+        ****/ }
+        <ScrollElement
+          style={{ height: 2000 }}
+          id="Scroll-Pack"
+          className="last"
+        >
+          <ScrollParallax
+            className="pack-page"
+            location="Scroll-Pack"
+            animation={{
+              backgroundColor: '#0097D0',
+              playScale: [1, 2],
+              onStart: () => {
+                this.setCss('start')
+              },
+              onCompleteBack: () => {
+                this.setCss('back complete')
+              },
+              onComplete: () => {
+                this.onComplete('complete')
+              },
+              onStartBack: () => {
+                this.onComplete('back start')
+              }
+            }}
+            style={this.state.css}
+            className="last__inner"
+          >
+            <ScrollParallax
+              animation={{ translateX: '0%', playScale: [1, 2] }}
+              style={{
+                transform: 'translateX(-100%)',
+                backgroundColor: '#F38EAD',
+                width: '100vw',
+                height: '100vh',
+                position: 'absolute'
+              }}
+              location="Scroll-Pack"
+            />
+            <ScrollParallax
+              animation={[{ translateY: 0 }, { translateY: '450px' }]}
+              location="Scroll-Pack"
+              style={{
+                transform: 'translateY(300px)',
+                color: '#fff',
+                fontSize: 36,
+                textAlign: 'center'
+              }}
+            >
+              Title 3
+            </ScrollParallax>
+          </ScrollParallax>
+        </ScrollElement>
       </div>
     )
   }
